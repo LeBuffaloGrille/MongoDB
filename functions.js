b04486675dced9a9049f054799c78f2b5176997c
LAPTOP-6JTA04B5\School <matteo.bienaime@mewo-campus.fr>
2022-03-23 17:38:33 +0100

    📦 NEW: check if the number is even or odd

--- functions.js
+++ functions.js
@@ -1,6 +1,24 @@
+// Déclaration de la variable "volume"
 var volume;
 
+// Fonction : Calcul du volume d'un cône
 export function cone_volume(radius, height) {
+    // Calcul du volume d'un cône
     volume = radius * radius * height * 3.14 * (1/3); 
+    // Affichage du volume calculé
     console.log(volume);
+}
+
+// Fonction : Pair ou impair
+export function peer_or_odd(checkPeerOrOdd){
+    // Si le chifre / nombre est impair
+    if(checkPeerOrOdd % 2 == 1){
+        // Affichage de la valeur impair puis indication du status
+        console.log(checkPeerOrOdd + " is odd");
+    }
+    // Si le chifre / nombre est pair
+    else{
+        // Affichage de la valeur pair puis indication du status
+        console.log(checkPeerOrOdd + " is peer");
+    }
 }
--- main.js
+++ main.js
@@ -1,3 +1,4 @@
-import { cone_volume } from './functions.js';
+import { cone_volume, peer_or_odd } from './functions.js';
 
 cone_volume(10.00,10.00);
+peer_or_odd(3);
